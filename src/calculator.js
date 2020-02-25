ff45dff8f654263918374c75644e8c37579d0607
lmachens <leon.machens@googlemail.com>
2020-02-25 16:35:54 +0100

    Add click events

--- src/calculator.js
+++ src/calculator.js
@@ -1,1 +1,9 @@
-document.querySelector(".calculator__output").value = "42";
+const calculatorOutput = document.querySelector(".calculator__output");
+calculatorOutput.value = "42";
+
+const calculatorInputs = document.querySelectorAll(".calculator__input");
+calculatorInputs.forEach(function(calculatorInput) {
+  calculatorInput.addEventListener("click", function() {
+    calculatorOutput.value = calculatorInput.innerHTML;
+  });
+});
